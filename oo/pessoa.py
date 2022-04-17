f3279a5b1e374d07d8eff05157fe151cf0ea9134
Phernando <nandovalverde@gmail.com>
2022-04-17 17:32:57 +0100

    Criado e removido atributo dinâmico de objeto

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -15,7 +16,18 @@ if __name__ == '__main__':
     print(rubens.cumprimentar())
     print(id(rubens))
     print(rubens.nome)
-    rubens.nome = 'Fernando'
-    print(rubens.nome)
     print(rubens.idade)
-    print(rubens.filhos)
+    for filho in rubens.filhos:
+        print(filho.nome)
+    rubens.sobrenome = 'Ramalho'
+    print(rubens.__dict__)
+    print(fernando.__dict__)
+    del rubens.sobrenome
+    rubens.olhos = 1
+    print(rubens.__dict__)
+    print(Pessoa.olhos)
+    print(rubens.olhos)
+    print(fernando.olhos)
+    print(id(Pessoa.olhos), id(rubens.olhos), id(fernando.olhos))
+
+
