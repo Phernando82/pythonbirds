369dce3556e92ba1c5f0ec94d48ee30660fa120f
Phernando <nandovalverde@gmail.com>
2022-04-19 21:47:27 +0100

    Criado o atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -12,10 +13,21 @@ if __name__ == '__main__':
     fernando = Pessoa(nome='Fernando')
     rubens = Pessoa(fernando, nome='Rubens')
     print(Pessoa.cumprimentar(rubens))
-    print(rubens.cumprimentar())
     print(id(rubens))
-    print(rubens.nome)
-    rubens.nome = 'Fernando'
+    print(rubens.cumprimentar())
     print(rubens.nome)
     print(rubens.idade)
+    for filho in rubens.filhos:
+        print(filho.nome)
+    rubens.sobrenome = 'Ramalho'
     print(rubens.filhos)
+    del rubens.filhos
+    rubens.olhos = 1
+    del rubens.olhos
+    print(fernando.__init__)
+    print(rubens.__init__)
+    print(Pessoa.olhos)
+    print(rubens.olhos)
+    print(fernando.olhos)
+    print(id(Pessoa.olhos), id(rubens.olhos), id(fernando.olhos))
+
