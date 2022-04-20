20c4ea67e4c1aef745dd08936acd4018ccfb094e
Phernando <nandovalverde@gmail.com>
2022-04-20 22:10:44 +0100

    Criado o atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Hola! {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     fernando = Pessoa(nome='Fernando')
@@ -30,4 +38,6 @@ if __name__ == '__main__':
     print(rubens.olhos)
     print(fernando.olhos)
     print(id(Pessoa.olhos), id(rubens.olhos), id(fernando.olhos))
+    print(Pessoa.metodo_estatico(), rubens.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), rubens.nome_e_atributos_de_classe())
 
