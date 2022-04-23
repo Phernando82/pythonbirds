06caf75e555379534c5a6ed1a8b5fd53cf2beb24
Phernando <nandovalverde@gmail.com>
2022-04-23 14:48:25 +0100

    Reutilização de código por herança para implementação dos dois tipos de pássaros: Amarelo e Vermelho

--- atores.py
+++ atores.py
@@ -139,8 +139,11 @@ class Passaro(Ator):
 
 
 class PassaroAmarelo(Passaro):
-    _caracter_ativo = 'A'
+    # _caracter_ativo = 'A'
+    pass
 
 
 class PassaroVermelho(Passaro):
     _caracter_ativo = 'V'
+    _caracter_destruido = 'v'
+    velocidade_escalar = 20
